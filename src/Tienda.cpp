0a07c23b2145ea1727c32727a829b35fe2fc7668
SantiagoMurillas <smurillas9@icloud.com>
2025-03-19 10:19:33 -0500

    Añadido Definicion de Metodos Lista productos y Lista Clientes

--- src/Tienda.cpp
+++ src/Tienda.cpp
@@ -11,17 +11,22 @@ void Tienda::mostrarVentas() {
 }
 
 void Tienda::mostrarListaProductos() {
-
+       for (int i=0;i<productos.size();i++) {
+           productos[i].mostrarInfo();
+       }
 }
 
 void Tienda::registrarVenta(int idCliente) {
+
 
 }
 
 //float Tienda::calcularTotalInv() {}
 
 void Tienda::mostrarListaClientes() {
-
+    for (int i=0;i<clientes.size();i++) {
+        clientes[i].mostrarInfo();
+    }
 }
 
 void Tienda::reabastecerProducto(std::string codigoProducto, int cantidad) {
